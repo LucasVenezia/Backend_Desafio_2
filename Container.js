55e217b6397760acc3e5320be4e7a44a8d8ed5bd
Lucas Venezia <lucasavenezia@gmail.com>
2022-01-25 15:25:01 -0300

    Primer commit

---
+++ Container.js
@@ -0,0 +1,78 @@
+const fs = require ('fs');
+
+/* Schema
+        product ={
+            title:String (required),
+            price:Number (required),
+            thumbnail: String
+        }
+*/
+
+const pathToProducts = './files/products.txt'
+
+class container{
+    saveProduct = async (product) =>{
+        if(!product.title||!product.price) return {status:"error", error: "missing data"}
+        try{
+            if(fs.existsSync(pathToProducts)){
+                let data = await fs.promises.readFile(pathToProducts, 'utf-8')
+                let products = JSON.parse(data);
+                if (products.length === 0){
+                    product.id = 1;
+                }else{
+                    let id = products[products.length-1].id+1;
+                    product.id = id;
+                }
+                products.push(product);
+                await fs.promises.writeFile(pathToProducts,JSON.stringify(products,null,2))
+                return {status:"success",message:"Product inserted"}
+            }else{
+                product.id = 1
+                await fs.promises.writeFile(pathToProducts,JSON.stringify([product],null,2))
+                return {status:"success",message:"Product inserted"}
+            }   
+        }catch(error){
+            return {status: "error",message:error}
+        }
+    }
+    getall = async () => {
+        if(fs.existsSync(pathToProducts)){
+            let data = await fs.promises.readFile(pathToProducts, 'utf-8')
+            let products = JSON.parse(data);
+            return {status: "success", productList:products}
+        }
+    }
+    getById = async (id) => {
+        if(!id) return {status:"error", error:"Missing Id"};
+        if(fs.existsSync(pathToProducts)){
+            let data = await fs.promises.readFile(pathToProducts, 'utf-8')
+            let products = JSON.parse(data);
+            let product = products.find(p =>p.id===id);
+            if (product) return {status:"success", productList:product} 
+            else return {status:"error", error:"Product not found"}
+        }
+    }
+    deleteById = async (id) =>{
+        if(!id) return {status:"error", error:"Missing Id"};
+        if(fs.existsSync(pathToProducts)){
+            let data = await fs.promises.readFile(pathToProducts, 'utf-8')
+            let products = JSON.parse(data);
+            let newProducts = products.filter(p =>p.id!==id)
+            await fs.promises.writeFile(pathToProducts,JSON.stringify(newProducts,null,2))
+            return {status: "success", message:"Product deleted"}
+        }
+    }
+    deleteAll = async () => {
+        if(fs.existsSync(pathToProducts)){
+            let data = await fs.promises.readFile(pathToProducts, 'utf-8')
+            let products = JSON.parse(data);
+            let emptyList = products
+            emptyList.splice(0, products.length); 
+            await fs.promises.writeFile(pathToProducts,JSON.stringify(emptyList,null,2)) 
+            return {status: "success", message:"List deleted"}      
+        }
+
+    }
+}
+
+module.exports = container;
---
+++ index.js
@@ -0,0 +1,36 @@
+const Container = require('./Container.js')
+
+const productService = new Container();
+
+// productos comentados, para ser insertados descomentar
+
+// let product = {
+//     title: "pinza",
+//     price: 150,
+//     thumbnail: "https://d3f64ghtnjizps.cloudfront.net/wp-content/uploads/sites/103/2020/08/10092833/hjbl-1.jpg"
+// }
+
+// let product = {
+//     title: "destornillador",
+//     price: 200,
+//     thumbnail: "https://pimdatacdn.bahco.com/media/sub624/16a0ffdce467453f.png"
+// }
+
+// let product = {
+//     title: "tenaza",
+//     price: 120,
+//     thumbnail: "https://pimdatacdn.bahco.com/media/sub848/16a7b2d4a739fd3f.png"
+// }
+
+
+// Llamada de metodos descomentar para utilizar
+
+//productService.saveProduct(product).then(result=>console.log(result))
+
+//productService.getall().then(result=>console.log(result))
+
+//productService.getById().then(result=>console.log(result))
+
+//productService.deleteById().then(result =>console.log(result))
+
+//productService.deleteAll().then(result =>console.log(result))
